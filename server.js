8aee1faf41baf504eccc533690df658eca2512fe
Mostafa Mahmoud <mostafa416202@fsc.bu.edu.eg>
2025-07-15 03:14:48 +0300

    init: app setup && DB config

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+const connectDB = require('./config/db');
+require('dotenv').config();
+connectDB();
+const PORT = process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`Server Running on http://localhost:${PORT}`)
+})
+ 

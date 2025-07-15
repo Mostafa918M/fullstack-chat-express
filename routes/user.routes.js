d819129e8b3f63d7566da79efc1b6d3c4be628a5
Mostafa Mahmoud <mostafa416202@fsc.bu.edu.eg>
2025-07-15 22:11:43 +0300

    add user signin and JWT token support

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,30 @@
-const app = require('../app')
-const express = require('express')
-const { signup, signin } = require('../controllers/user.controller')
+const app = require("../app");
+const express = require("express");
+const { signup, signin } = require("../controllers/user.controller");
+const { newAccessToken } = require("../utils/jwt");
+const { body, validationResult } = require("express-validator");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/signup',signup)
-router.post('/signin',signin)
+router.post(
+  "/signup",
+  [
+    body("username").notEmpty().withMessage("Username is required"),
+    body("email").isEmail().withMessage("Valid email is required"),
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters"),
+  ],
+  signup
+);
+router.post(
+  "/signin",
+  [
+    body("email").isEmail().withMessage("Valid email is required"),
+    body("password").notEmpty().withMessage("Password is required"),
+  ],
+  signin
+);
+router.post("/refresh-token", newAccessToken);
 
-module.exports = router
+module.exports = router;
---
+++ utils/jwt.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/users.model");
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+
+function generateRefrechToken(user) {
+  const payload = {
+    id: user._id,
+  };
+  return jwt.sign(payload, REFRESH_TOKEN_SECRET, { expiresIn: "30d" });
+}
+function generateAccessToken(user) {
+  const payload = {
+    id: user._id,
+  };
+  return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: "30d" });
+}
+
+function newAccessToken(req, res) {
+  const token = req.cookies.refresh_token;
+  if (!token) {
+    return res.status(401).json({ message: "No refresh token" });
+  }
+  try {
+    jwt.verify(token, REFRESH_TOKEN_SECRET);
+    const newAccessToken = generateAccessToken(User);
+    res.json({ accesToken: newAccessToken });
+  } catch {
+    res.status(403).json({ message: "Invalid refresh token" });
+  }
+}
+
+module.exports = { generateRefrechToken, generateAccessToken, newAccessToken };
